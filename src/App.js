2bf61978e05ddda28e8ff6308bbaef1890397296
GERMAN ARCINIEGAS <germanarciniegas@MacBook-Air-de-GERMAN.local>
2024-01-26 19:28:10 -0500

    Instagram prototipe educational intention

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/images/instagramLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
